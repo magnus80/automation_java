53a1a0f83bfcbcd622d4c949c255e439ee4667c7
magnus80 <zarechny58@gmail.com>
2016-02-22 22:02:26 +0300

    Добавлен метод в класс, вызывается из main

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,12 +1,9 @@
 public class MyFirstProgram{
 
 	public static void main(String[] args){
-		Point p1=new Point();
-		Point p2=new Point();
-		p1.x=45;
-		p1.y=-56;
-		p2.x=0;
-		p2.y=565;
+		Point p1=new Point(45,-56);
+		Point p2=new Point(0,565);
+		/*вызов через метод класса*/
 		System.out.println("Расстояние между двумя точками равно: "+  distance(p1,p2));
 	}
 	public static double distance(Point p1, Point p2){
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -5,5 +5,16 @@ public class Point {
  
     public int x;
     public int y;
+
+    public Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public static double distance(Point p1, Point p2){
+        int difX=p2.y-p1.y;
+        int difY=p2.x-p1.x;
+        return Math.sqrt((difX*difX)-(difY*difY));
+    }
 }
 
