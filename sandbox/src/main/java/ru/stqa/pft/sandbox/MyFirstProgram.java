c295733c61eed49febf23ca74052c86fca10888f
magnus80 <zarechny58@gmail.com>
2016-02-27 15:24:48 +0300

    написаны тесты для класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,15 +5,12 @@ public class MyFirstProgram{
 	public static void main(String[] args){
 		Point p1=new Point(45,-56);
 		Point p2=new Point(0,565);
+
 		/*вызов через метод класса*/
 		System.out.println("Расстояние между двумя точками равно: "+  Point.distance(p1,p2));
 	}
 	/*public static double distance(ru.stqa.pft.sandbox.Point p1, ru.stqa.pft.sandbox.Point p2){
 		int difX=p2.y-p1.y;
-	@FindBy(css="")
-	private WebElement webElement;
-	@FindBy(css="")
-	private WebElement webElement;
 		int difY=p2.x-p1.x;
 		return Math.sqrt((difX*difX)-(difY*difY));
 	}*/
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,10 +1,31 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * Created by KIryshkov on 25.02.2016.
  */
 public class PointTests {
-    Point p1=new Point(56,98);
-    Point p2=new Point(-45,-98);
+
+    @Test
+    public void testDistance1(){
+        Point p1=new Point(5,-98);
+        Point p2=new Point(57,99);
+        Assert.assertEquals(Point.distance(p1,p2),190.01315743916263);
+    }
+    @Test
+    public void testDistance2(){
+        Point p1=new Point(-9,435);
+        Point p2=new Point(67,5);
+        Assert.assertEquals(Point.distance(p1,p2),423.23043368831594);
+    }
+    @Test
+    public void testDistance3(){
+        Point p1=new Point(79,28);
+        Point p2=new Point(85,78);
+        Assert.assertEquals(Point.distance(p1,p2),49.63869458396343);
+    }
+
 
 }
