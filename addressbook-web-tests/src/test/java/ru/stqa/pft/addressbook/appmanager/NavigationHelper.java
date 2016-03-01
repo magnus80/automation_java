6ef38dc1a7ffd414b2ffac2dcbd43ee51dca2db5
magnus80 <zarechny58@gmail.com>
2016-03-02 01:05:25 +0300

    Создан общий базовый класс BaseHelper
    Создать для этих вспомогательных классов общий базовый класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Homer-PC on 01.03.2016.
  */
-public class NavigationHelper {
-    private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd=wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-      wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
   }
 
     public void gotoHomePage() {
-        wd.findElement(By.linkText("home")).click();
+        click(By.linkText("home"));
     }
 }
