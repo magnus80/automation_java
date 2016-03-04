7de50fe24c96c72c4003bd4d870e5f5d7d8e1122
magnus80 <zarechny58@gmail.com>
2016-03-04 16:49:28 +0300

    написан запуск с браузреа Хром

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,17 +12,25 @@ import java.util.concurrent.TimeUnit;
  * Created by Homer-PC on 01.03.2016.
  */
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
 
-
+    public ApplicationManager(String browser){
+        this.browser=browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser== BrowserType.FIREFOX){
+            wd = new FirefoxDriver();}
+        else if (browser== BrowserType.CHROME){
+            wd = new ChromeDriver();}
+        else if (browser== BrowserType.IE){
+            wd = new InternetExplorerDriver();}
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:81/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
