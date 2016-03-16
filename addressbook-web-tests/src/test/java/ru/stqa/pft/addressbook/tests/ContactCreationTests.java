81afa89d0f9c50e4a3e0562606f2c330bd97ff98
magnus80 <zarechny58@gmail.com>
2016-03-16 17:04:30 +0300

    добавлены лямбда-выраженния во все тесты.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -15,20 +15,14 @@ public class ContactCreationTests extends TestBase {
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before=app.getContactHelper().getContactList();
         app.getContactHelper().initContactCreation();
-        ContactData contact=new ContactData("Ivan", "I", "Ivanov", "Ivy", null, null, "Московская, 90", "56-90-90", "968-253-36-36", "56-56-56", "56-65-56", "ivan.ivanovi.@best.com");
+        ContactData contact=new ContactData("Ivano", "I", "Ivanov", "Ivy", null, null, "Московская, 90", "56-90-90", "968-253-36-36", "56-56-56", "56-65-56", "ivan.ivanovi.@best.com");
         app.getContactHelper().fillContactForm(contact);
         app.getContactHelper().submitContactCreation();
         app.getNavigationHelper().returnToHomePage();
         List<ContactData> after=app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-      int max=0;
-      for (ContactData g:after){
-        if (g.getId()>max){
-          max=g.getId();
-        }
-      }
-      contact.setId(max);
+      contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
       before.add(contact);
       Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
