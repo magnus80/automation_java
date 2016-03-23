9480100d250ab72f66856e4b0ec3f8139866db9b
magnus80 <zarechny58@gmail.com>
2016-03-24 02:03:05 +0300

    Создан пустой класс для 13 задания

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactComparisonDataTest.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.addressbook.tests;
+
+/**
+ * Created by Homer-PC on 24.03.2016.
+ */
+public class ContactComparisonDataTest extends TestBase {
+
+//    ясно, значит все что видно в списке на главной + миддлнейм - это обязательно, остальное - на усмотрение
+}
