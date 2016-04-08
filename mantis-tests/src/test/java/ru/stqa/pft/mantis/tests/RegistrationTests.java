840090645aa00d96c4a26bed5fd7c858c757fce3
magnus80 <zarechny58@gmail.com>
2016-04-08 16:15:16 +0300

    добавлена ленивая инициализация

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by KIryshkov on 08.04.2016.
+ */
+public class RegistrationTests extends TestBase {
+
+  @Test
+  public void tetsRegistration(){
+    app.registration().start("user1","user1@localhost.localdomain");
+  }
+}
